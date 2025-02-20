3af0e707c714b777d2d28497b1f94f11c1abf9e6
Yesid Lopez <yesid.leonardo.lopez@hotmail.com>
2025-02-20 11:50:27 +0100

    Hide iPhone frame on mobile devices using Tailwind responsive classes

--- src/components/features.tsx
+++ src/components/features.tsx
@@ -8,7 +8,7 @@ export function Features() {
       <div className="relative w-full">
         <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-[90%] h-[90%] sm:h-[70%] bg-black/90 rounded-[3rem]" />
         <div className="absolute inset-0 grid grid-cols-12 items-center">
-          <div className="col-span-12 sm:col-start-3 sm:col-span-4">
+          <div className="hidden sm:block col-span-12 sm:col-start-3 sm:col-span-4">
             <IPhoneFrame />
           </div>
         </div>
